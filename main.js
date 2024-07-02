6a94caafba4184f7e3464d7acd8a3e1ae4dac94f
Sharom Bromberg <sharombg7@gmail.com>
2024-07-02 17:15:37 -0500

    cart folder added, graphic interface in progress

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+import { menuInit } from "./src/app/components/header/menu";
 import { productsInit } from "./src/app/components/products/products"
 
 const init = () => {
     productsInit();
+    menuInit();
 }
 
 init();
--- src/app/components/header/menu.js
+++ src/app/components/header/menu.js
@@ -1,5 +1,5 @@
 const menuButton = document.querySelector('.toggle-menu');
-const barsMenu = document.querySelector('.navlist');
+const barsMenu = document.querySelector('.navitems');
 const overlay = document.querySelector('.overlay');
 const cartButton = document.querySelector('.toggle-cart');
 const cartMenu = document.querySelector('.cart-box');
@@ -27,4 +27,31 @@ const closeOnClick = (e) => {
     if (!e.target.classList.contains('link')) return;
     barsMenu.classList.remove('active-menu');
     overlay.classList.remove('active-overlay');
+}
+
+const closeOnScroll = () => {
+    if (
+        !barsMenu.classList.contains('active-menu')
+        && !cartMenu.classList.contains('active-cart')
+    )
+        return;
+    barsMenu.classList.remove('active-menu');
+    cartMenu.classList.remove('active-cart');
+    overlay.classList.remove('active-overlay');
+}
+
+const closeOnOverlayClick = () => {
+    barsMenu.classList.remove('active-menu');
+    cartMenu.classList.remove('active-cart');
+    overlay.classList.remove('active-overlay');
+
+}
+
+export const menuInit = () => {
+    menuButton.addEventListener('click', toggleMenu);
+    cartButton.addEventListener('click', toggleCart);
+    overlay.addEventListener('click', closeOnClick);
+    window.addEventListener('scroll', closeOnScroll);
+    barsMenu.addEventListener('click', closeOnClick);
+    overlay.addEventListener('click', closeOnOverlayClick);
 }
